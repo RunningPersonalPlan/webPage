30a57603c612acaa1cecf555002127af5835799d
nicolaserd <530nicolas@gmail.com>
2024-06-14 15:54:50 -0500

    fix: formularion con evio de correos

--- Rpp/src/views/Contacto.jsx
+++ Rpp/src/views/Contacto.jsx
@@ -1,8 +1,17 @@
 // ContactForm.js
 import React, { useState } from 'react';
 import './ContactUs.css';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import LoadingSpinner from '../components/Isloading';
 
 const ContactForm = () => {
+
+  const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [botonDesactivado, setBotonDesactivado] = useState(false);
+
   const [form, setForm] = useState({
     name: '',
     email: '',
@@ -23,8 +32,11 @@ const ContactForm = () => {
     setForm(nextFormState);
   };
 
-  const onSubmitForm = (e) => {
+  const onSubmitForm = async (e) => {
     e.preventDefault();
+    await setIsLoading(true);
+    setBotonDesactivado(true);
+
     const { name, email, message } = form;
     if (!name || !email || !message) {
       setErrors({
@@ -32,17 +44,50 @@ const ContactForm = () => {
         email: !email ? 'Required' : '',
         message: !message ? 'Required' : '',
       });
+     
+      setIsLoading(false);
+      setBotonDesactivado(false);
       return;
+      
     }
     if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
       setErrors({ email: 'Invalid email address' });
+      console.log(isLoading)
+      setIsLoading(false);
+      setBotonDesactivado(false);
       return;
     }
     // Submit the form data here
     console.log('Form submitted:', form);
+
+    const emailData = {
+      to: form.email,
+      subject: "Solicitudes contacto",
+      text: form.message,
+      name: form.name
+    };
+
+    await axios.post('https://backweppage.vercel.app/send-email', emailData)
+    .then(response => {
+    
+      setBotonDesactivado(false);
+      navigate('/');
+      
+      
+    })
+    .catch(error => {
+        
+        navigate('/');
+        
+        
+    });
+    setIsLoading(false);
   };
 
   return (
+    
+    <>
+    {!isLoading && (
     <div className="container">
       <h2 className="title">Contacto</h2>
       <form className="form" onSubmit={onSubmitForm}>
@@ -78,9 +123,12 @@ const ContactForm = () => {
         />
         {errors.message && <div className="error">{errors.message}</div>}
         <br />
-        <input className="submit" type="submit" value="Enviar" />
+        <input className="submit" type="submit" disabled={botonDesactivado} value="Enviar" />
       </form>
-    </div>
+    </div>)}
+    {isLoading && <LoadingSpinner />}
+    </>
+    
   );
 };
 
