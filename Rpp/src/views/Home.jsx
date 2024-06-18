0aef7aa4140943e7b1b527048d1d4885db158cd0
nicolaserd <530nicolas@gmail.com>
2024-06-17 23:37:29 -0500

    fix: home  v1

--- Rpp/src/views/Home.jsx
+++ Rpp/src/views/Home.jsx
@@ -1,33 +1,6 @@
 import React from 'react';
 import "./Home.css"
 import Primary from "../assets/Primary.png"
-import imgAbout1 from "../assets/ImgAboutUs.jpg"
-import TestimonioCard from '../components/TestimonioCard.jsx';
-
-const testimonios = [
-  {
-    nombre: 'Juan Pérez',
-    deporte: 'Fútbol',
-    profesion: 'Jugador Profesional',
-    descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis ligula quis porttitor blandit.',
-    imagen: 'https://via.placeholder.com/300', // URL de la imagen del testimonio
-  },
-  {
-    nombre: 'María García',
-    deporte: 'Tenis',
-    profesion: 'Entrenadora',
-    descripcion: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-    imagen: 'https://via.placeholder.com/300', // URL de la imagen del testimonio
-  },
-  {
-    nombre: 'María García',
-    deporte: 'Tenis',
-    profesion: 'Entrenadora',
-    descripcion: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-    imagen: 'https://via.placeholder.com/300', // URL de la imagen del testimonio
-  },
-];
-
 
 const Home = () => {
 
@@ -66,39 +39,24 @@ const Home = () => {
        
       </section>
     </section>
-    <section className="seccionComunH">
-      <aside className='asideText'>
-        <h1 className='text-yellow-300'>Sobre Nosotros</h1>
-        <p>
-        En Running Personal Plan, somos un equipo de profesionales convencidos del poder transformador 
-        del running y comprometidos con el bienestar de las personas. Creemos que esta disciplina no 
-        solo mejora la condición física, sino que también tiene un impacto positivo en la salud mental, 
-        la autoestima y el bienestar general.
-        </p>
-      </aside>
-
-      <aside className='asideText containerImg'>
-        <img src={imgAbout1} className='aboutUsImg'/>
-      </aside>      
-
-    </section>
-
-    <div className="mt-20">
-      <h1 className="text-3xl font-bold mb-8 text-center text-yellow-300">Testimonios</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {/* Mapeamos cada testimonio para mostrarlo utilizando TestimonioCard */}
-        {testimonios.map((testimonio, index) => (
-          <TestimonioCard
-            key={index}
-            nombre={testimonio.nombre}
-            deporte={testimonio.deporte}
-            profesion={testimonio.profesion}
-            descripcion={testimonio.descripcion}
-            imagen={testimonio.imagen}
-          />
-        ))}
+    <section className='servicios'>
+      <div className='container-titulos'>
+        <h1 className='tiulo-sevicios'>Estos son los servicios que ofrecemos</h1>
+        <h2 className='subtitulo-servicios'>Entrenamiento personalizados y apoyo continuo para alcanzar tus metas deportivas</h2>
       </div>
-    </div>
+      <div className="container-servicios">
+        <div className='e-personal'>
+          <h1>ENTRENAMIENTO PERSONAL Y AUTÓNOMO</h1>
+          <h2>Nos adaptamos a tus necesidades. Entrena a tu ritmo y logra tus metas deportivas con nuestro apoyo.</h2>
+        </div>
+        <div className='e-personal1'>
+          <h1>MICRO CICLOS</h1>
+          <h2>Incluyen entrenamientos de flexibilidad, fuerza y cross training. Diseñados para mejorar tu rendimiento integral y prevenir lesiones.
+          </h2>
+        </div>
+      </div>
+      
+    </section>
     
     </>
  
