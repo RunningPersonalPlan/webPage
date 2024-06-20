cf56ffbd4e7d8da80143bc84ae285f56c578b7d4
nicolaserd <530nicolas@gmail.com>
2024-06-20 13:09:57 -0500

    fix: responsive sobre nosotros

--- Rpp/src/views/Entrenamiento.jsx
+++ Rpp/src/views/Entrenamiento.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import NuestroEquipoCardIzq from "../components/NuestroEquipoCardIzq.jsx"
 import eduardoimg from "../assets/Eduardoimg.png";
 import Monica from "../assets/Monica.png";
+import Nicolas from "../assets/Nicolas.png"
+import OscarAndres from "../assets/OscarAndres.png"
 import "./NuestroEquipo.css"
 import NuestroEquipoCardDer from '../components/NuestroEquipoCardDer.jsx';
 const Entrenamiento = () => {
@@ -75,6 +77,98 @@ const Entrenamiento = () => {
 
         <div className='area-1 mb-8 mx-8 md:mx-8 sm:mx-0 mt-8' >
           <h1 className='titulo-area-1 mb-8' >CEO / CO FOUNDER</h1>
+          <p className='content-area-1'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam dolore, tenetur
+             tempore accusamus nemo quasi. Aliquam nulla id rerum eos aliquid assumenda, 
+             repudiandae, perferendis, explicabo a repellendus ex. Sit, suscipit. Lorem ipsum 
+            
+          </p>
+
+        </div>
+
+        <div className='area-1 mb-8 mx-8 md:mx-8 sm:mx-0 mt-8'>
+          
+            <h1 className='titulo-area-1 mb-8 '>LOGROS DEPORTIVOS Y FORMACIÓN</h1>
+            <div className='content-area-1'>
+               <ul className='list-disc pl-5 space-y-2 content-area-1'>
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut sunt culpa, ea</li>
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam asperiore</li>
+                  <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam ab suscipit</li>
+                  <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non suscipit neque</li>
+               </ul>
+            </div>
+       
+        </div>
+        </div>
+        
+      </section>
+
+      <section className='flex flex-col justify-center'>
+        <div className="flex flex-wrap justify-center gap-6">
+        
+          <NuestroEquipoCardIzq
+            key="NICOLÁS INCHASUTEGUI"
+            nombre="NICOLÁS"
+            apellido="I. GONZÁLEZ"
+            imagen={Nicolas}
+          />
+    
+        </div> 
+        <div className='area-1 mb-8 mx-8 md:mx-8 sm:mx-0 mt-8' >
+          <h1 className='titulo-area-1 mb-8' >DESARROLLADOR WEB</h1>
+          <p className='content-area-1'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam dolore, tenetur
+             tempore accusamus nemo quasi. Aliquam nulla id rerum eos aliquid assumenda, 
+             repudiandae, perferendis, explicabo a repellendus ex. Sit, suscipit. Lorem ipsum 
+          </p>
+
+        </div>
+
+        <div className='flex-area-1 flex gap-8  mx-8 md:mx-8 sm:mx-0 '>
+          <div className='flex-area-1-content'>
+            <h1 className='flex-titulo-area-1 mb-8 '>LOGROS DEPORTIVOS</h1>
+            <div className='flex-content-area-1'>
+               <ul className='list-disc pl-5 space-y-2'>
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut sunt culpa, ea</li>
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam asperiore</li>
+                  <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam ab suscipit</li>
+                  <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non suscipit neque</li>
+               </ul>
+            </div>
+          </div>
+          <div className='flex-area-1-content'>
+            <h1 className='flex-titulo-area-1 mb-8'>FORMACIÓN</h1>
+            <div className='flex-content-area-1'>
+            <ul className='list-disc pl-5 space-y-2'>
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut sunt culpa, ea</li>
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam asperiore</li>
+                  <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam ab suscipit</li>
+                  <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non suscipit neque</li>
+               </ul>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+      <section className='flex  justify-center mt-8 sectionizq'>
+        <div className="flex  justify-center gap-6">
+        
+          <NuestroEquipoCardDer
+            key="ANDRÉS GONZÁLEZ"
+            nombre="ANDRÉS"
+            apellido="GONZÁLEZ"
+            imagen={OscarAndres}
+          />
+    
+        </div> 
+
+        <div className='containerder'>
+
+        <div className='area-1 mb-8 mx-8 md:mx-8 sm:mx-0 mt-8' >
+          <h1 className='titulo-area-1 mb-8' >DIRECTOR CREATIVO
+
+</h1>
           <p className='content-area-1'>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam dolore, tenetur
              tempore accusamus nemo quasi. Aliquam nulla id rerum eos aliquid assumenda, 
