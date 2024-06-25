838c31c91a85d69fcd82ee29b1f5851ceed2df89
nicolaserd <530nicolas@gmail.com>
2024-06-25 06:45:33 -0500

    add: titulo blog

--- Rpp/src/views/Blog.jsx
+++ Rpp/src/views/Blog.jsx
@@ -1,42 +1,12 @@
 import React from 'react';
-import BlogComponent from '../components/BlogComponent.jsx';
-
-const activities = [
-  {
-    activity: 'Fútbol',
-    title: 'Gran victoria del equipo local en el torneo',
-    image: 'https://via.placeholder.com/300',
-    description: 'El equipo local logró una impresionante victoria en el torneo regional, destacando con un marcador de 3-1 contra su mayor rival. El partido estuvo lleno de emoción y momentos destacados...',
-  },
-  {
-    activity: 'Baloncesto',
-    title: 'Partido emocionante en la liga de baloncesto',
-    image: 'https://via.placeholder.com/300',
-    description: 'El partido de baloncesto fue un espectáculo, con ambos equipos dando lo mejor de sí. El marcador final fue 95-93, un juego muy reñido...',
-  },
-  {
-    activity: 'Tenis',
-    title: 'Torneo de tenis atrae a grandes talentos',
-    image: 'https://via.placeholder.com/300',
-    description: 'El torneo de tenis de este año ha atraído a jugadores de alto nivel. Las semifinales fueron intensas, y el público disfrutó cada momento...',
-  }
-];
+import "./Blog.css"
 
 const Blog = () => {
   return (
     <div className="flex flex-col items-center py-10 px-5 sm:px-10 lg:px-20">
-      <h1 className="text-yellow-400 text-3xl font-bold py-8 mt-10">Actividades</h1>
-      <div className="flex flex-wrap justify-center">
-        {activities.map((activity, index) => (
-          <BlogComponent
-            key={index}
-            activity={activity.activity}
-            title={activity.title}
-            image={activity.image}
-            description={activity.description}
-          />
-        ))}
-      </div>
+      <h1 className="titulo-pupilos-black">ALGUNOS DE </h1>
+      <h1 className='titulo-pupilos-yellow'>NUESTROS PUPILOS</h1>
+     
     </div>
   );
 };
