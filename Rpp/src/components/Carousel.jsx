7e13c206b8e9cf29fc68e54de05060b164fca1c1
nicolaserd <530nicolas@gmail.com>
2024-06-28 16:24:53 -0500

    carrousel mejorado con mouse whell

--- Rpp/src/components/Carousel.jsx
+++ Rpp/src/components/Carousel.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Mousewheel,EffectFade} from 'swiper/modules';
-import { Scrollbar} from 'swiper/modules';
+import { Autoplay, Mousewheel,EffectFade,Pagination} from 'swiper/modules';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/scrollbar';
+import 'swiper/css/pagination';
 import 'swiper/css/effect-fade';
 import "swiper/css/mousewheel"
+import "swiper/css/autoplay"
 import './Carousel.css';
 
 const Carousel = ({ slides }) => {
@@ -25,8 +25,11 @@ const Carousel = ({ slides }) => {
     
     autoplay={{ delay: 5000, disableOnInteraction: false }}
     mousewheel={{ forceToAxis: true }}
-    scrollbar={{ draggable: true }}
-    modules={[Autoplay, Mousewheel,Scrollbar,EffectFade]} 
+    pagination={{
+      clickable: true,
+    }}
+    
+    modules={[Autoplay, Mousewheel,EffectFade,Pagination]} 
 
     onSlideChange={() => console.log('slide change')}
     onSwiper={(swiper) => console.log(swiper)}
