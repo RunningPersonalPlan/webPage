6056c1abc567ca29ff1b34542a216e267bb0a890
nicolaserd <530nicolas@gmail.com>
2024-07-04 21:49:53 -0500

    fix: accordeon componente suavizado

--- Rpp/src/components/Home/Accordion.jsx
+++ Rpp/src/components/Home/Accordion.jsx
@@ -1,14 +1,39 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './Accordion.css';
 import PropTypes from 'prop-types';
-import Open from "../../assets/homelogo/arrow-up.svg"
-import Close from "../../assets/homelogo/arrow-down.svg"
+import Open from "../../assets/homelogo/arrow-up.svg";
+import Close from "../../assets/homelogo/arrow-down.svg";
+
 const Accordion = ({ title, content }) => {
     const [isOpen, setIsOpen] = useState(false);
+    const contentRef = useRef(null); // Crear una referencia
 
     const toggleAccordion = () => {
         setIsOpen(!isOpen);
     };
+
+    useEffect(() => {
+        if (contentRef.current) {
+            if (isOpen) {
+                contentRef.current.style.maxHeight = contentRef.current.scrollHeight + "px"; // Establecer maxHeight
+                contentRef.current.style.visibility = "visible";
+                contentRef.current.style.opacity = "1";
+            } else {
+                contentRef.current.style.maxHeight = "0px";
+                contentRef.current.style.visibility = "hidden";
+                contentRef.current.style.opacity = "0";
+            }
+        }
+    }, [isOpen]); // Efecto que se ejecuta cuando isOpen cambia
+
+    // Efecto para inicializar el maxHeight cuando el componente se monta
+    useEffect(() => {
+        if (contentRef.current) {
+            contentRef.current.style.maxHeight = isOpen ? contentRef.current.scrollHeight + "px" : "0px";
+            contentRef.current.style.visibility = isOpen ? "visible" : "hidden";
+            contentRef.current.style.opacity = isOpen ? "1" : "0";
+        }
+    }, []); // Efecto que se ejecuta al montar el componente
 
     return (
         <div className="accordion-item">
@@ -18,22 +43,21 @@ const Accordion = ({ title, content }) => {
                     <div className='isopen-img'>
                         <img src={isOpen ? Open : Close} alt='logo isopen?' />
                     </div>
-                   
                 </div>
             </button>
-            {isOpen && (
-                <div 
-                className="accordion-content"
-                dangerouslySetInnerHTML={{ __html: content }}>
-                    
-                </div>
-            )}
+            <div 
+                ref={contentRef} // Asignar la referencia al elemento
+                className={`accordion-content ${isOpen ? 'open' : ''}`}
+                dangerouslySetInnerHTML={{ __html: content }}
+            >
+            </div>
         </div>
     );
 };
+
 Accordion.propTypes = {
   title: PropTypes.string,
   content: PropTypes.string,
- 
 };
+
 export default Accordion;
